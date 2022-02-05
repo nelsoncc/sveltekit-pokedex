be850da2d69e095247fb3692e1a17062c7d77d66
Nelson Correia <nelson.cintra.c@gmail.com>
2022-02-05 17:55:57 +0000

    Display Pokemon Cards as grid using CSS

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,6 +8,8 @@
 </svelte:head>
 <h1 class="text-4xl text-center my-8 uppercase">SvelteKit Pokedex</h1>
 
-{#each $pokemons as pokemon}
-    <PokemonCard pokemon="{pokemon}"/>
-{/each}
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+    {#each $pokemons as pokemon}
+        <PokemonCard pokemon="{pokemon}"/>
+    {/each}
+</div>
