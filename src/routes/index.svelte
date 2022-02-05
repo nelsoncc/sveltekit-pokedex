311c2b6ac19160ef129f6db3a7182e8d39a6c941
Nelson Correia <nelson.cintra.c@gmail.com>
2022-02-05 16:14:54 +0000

    Move Nav component into layout page in order to be used in every Pages (children)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,7 @@
 <script>
-  import Nav from "../components/nav.svelte";
   console.log("Hello World!")
 </script>
 <svelte:head>
     <title>SvelteKit Pokedex</title>
 </svelte:head>
-<Nav/>
 <h1>SvelteKit Pokedex</h1>
