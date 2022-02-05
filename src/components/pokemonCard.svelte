cbe11697915e19a65de75c042987dc55b3477a06
Nelson Correia <nelson.cintra.c@gmail.com>
2022-02-05 17:58:54 +0000

    Make image and text from pokemonCard bigger

--- src/components/pokemonCard.svelte
+++ src/components/pokemonCard.svelte
@@ -3,6 +3,6 @@
 </script>
 <a class="p-6 bg-gray-100 text-gray-800 text-center rounded-md
 shadow-sm hover:shadow-md flex flex-col items-center" href={`/pokemon/${pokemon.id}`}>
-    <img src="{pokemon.image}" alt="{pokemon.name}">
-    <h2>{pokemon.id}. {pokemon.name}</h2>
+    <img class="h-40 w-40" src="{pokemon.image}" alt="{pokemon.name}">
+    <h2 class="uppercase text-2xl">{pokemon.id}. {pokemon.name}</h2>
 </a>
