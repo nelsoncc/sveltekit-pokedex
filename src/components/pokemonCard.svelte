b0b1cc0b3bdcbf0ca1a78a7b578add843fa798a5
Nelson Correia <nelson.cintra.c@gmail.com>
2022-02-05 17:54:41 +0000

    Add CSS classes to pokemonCard

--- src/components/pokemonCard.svelte
+++ src/components/pokemonCard.svelte
@@ -1,7 +1,8 @@
 <script>
   export let pokemon;
 </script>
-<a href={`/pokemon/${pokemon.id}`}>
+<a class="p-6 bg-gray-100 text-gray-800 text-center rounded-md
+shadow-sm hover:shadow-md flex flex-col items-center" href={`/pokemon/${pokemon.id}`}>
     <img src="{pokemon.image}" alt="{pokemon.name}">
     <h2>{pokemon.id}. {pokemon.name}</h2>
 </a>
