84e8b1797c3ff9156ad63c6569f82cf39d08806d
Nelson Correia <nelson.cintra.c@gmail.com>
2022-02-05 18:04:38 +0000

    Add search bar

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,17 @@
 <script>
   import {pokemons} from "../stores/pokestore";
   import PokemonCard from "../components/pokemonCard.svelte";
+  let searchPokemon = "";
+
   console.log($pokemons);
 </script>
 <svelte:head>
     <title>SvelteKit Pokedex</title>
 </svelte:head>
 <h1 class="text-4xl text-center my-8 uppercase">SvelteKit Pokedex</h1>
+
+<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
+       bind:value={searchPokemon}  type="text" placeholder="Search Pokemon">
 
 <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
     {#each $pokemons as pokemon}
