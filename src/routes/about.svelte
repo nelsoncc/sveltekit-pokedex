1a41f7595edc2497fbd6521d4dbf63af32d656e8
Nelson Correia <nelson.cintra.c@gmail.com>
2022-02-05 15:50:45 +0000

    Add svelte:head to index and about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,2 +1,5 @@
+<svelte:head>
+    <title>About SvelteKit Pokedex</title>
+</svelte:head>
 <h1>About</h1>
 <p>My first sveltekit application that uses the Pokemon API to build a Pokedex</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,7 @@
 <script>
   console.log("Hello World!")
 </script>
+<svelte:head>
+    <title>SvelteKit Pokedex</title>
+</svelte:head>
 <h1>SvelteKit Pokedex</h1>
