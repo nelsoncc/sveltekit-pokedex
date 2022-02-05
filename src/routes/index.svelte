d3afdfcb4d7d825f31544e4ba81e755a041c6117
Nelson Correia <nelson.cintra.c@gmail.com>
2022-02-05 15:57:04 +0000

    Add navigation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,9 @@
 <script>
+  import Nav from "../components/nav.svelte";
   console.log("Hello World!")
 </script>
 <svelte:head>
     <title>SvelteKit Pokedex</title>
 </svelte:head>
+<Nav/>
 <h1>SvelteKit Pokedex</h1>
