0a1ea13bee0c9fc8db570cc63dd8013008655757
Nelson Correia <nelson.cintra.c@gmail.com>
2022-02-05 16:50:12 +0000

    render pokemons name in main page
    - Removed Hello World log
    - log pokemons fetched data

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,12 @@
 <script>
-  console.log("Hello World!")
+  import {pokemons} from "../stores/pokestore";
+  console.log($pokemons);
 </script>
 <svelte:head>
     <title>SvelteKit Pokedex</title>
 </svelte:head>
 <h1>SvelteKit Pokedex</h1>
+
+{#each $pokemons as pokemon}
+    <p>{pokemon.name}</p>
+{/each}
