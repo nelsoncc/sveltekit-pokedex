e1a9b527a3e0c8267afbcc064031b74402bcff12
Nelson Correia <nelson.cintra.c@gmail.com>
2022-02-06 15:30:48 +0000

    Render more pokemon data in details pages

--- src/routes/pokemon/[id].svelte
+++ src/routes/pokemon/[id].svelte
@@ -9,5 +9,14 @@
 </script>
 <script>
   export let pokemon;
+  const type = pokemon.types[0].type.name;
 </script>
-<h1 class="text-4xl text-center my-8 uppercase">{pokemon.name}</h1>
+<div class="flex flex-col items-center">
+    <h1 class="text-4xl text-center my-8 uppercase">{pokemon.name}</h1>
+    <p>
+        Type: <strong>{type}</strong>
+        | Height: <strong>{pokemon.height}</strong>
+        | Weight: <strong>{pokemon.weight}</strong>
+    </p>
+    <img class="card-image" src="{pokemon.sprites['front_default']}" alt="{pokemon.name}">
+</div>
