1fbad43b421f64b102c8f4133501139ac1d4ab5f
Nelson Correia <nelson.cintra.c@gmail.com>
2022-02-06 15:17:44 +0000

    Add SSR in order to fetch Pokemon data, before pokemon page is rendered. Display pokemon name.

---
+++ src/routes/pokemon/[id].svelte
@@ -0,0 +1,13 @@
+<script context="module">
+  export async function load({params}) {
+    const id = params.id;
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const result = await fetch(url);
+    const pokemon = await result.json();
+    return {props: {pokemon}};
+  }
+</script>
+<script>
+  export let pokemon;
+</script>
+<h1 class="text-4xl text-center my-8 uppercase">{pokemon.name}</h1>
